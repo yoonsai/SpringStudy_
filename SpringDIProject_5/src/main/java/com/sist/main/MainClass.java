fa267930585616b3b16feba8a73aaeca077f1c74
yoonsai <micelle0716@naver.com>
2024-01-24 17:29:06 +0900

    DI5

---
+++ SpringDIProject_5/src/main/java/com/sist/main/MainClass.java
@@ -0,0 +1,21 @@
+package com.sist.main;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.sist.dao.EmpDAO;
+import com.sist.dao.EmpVO;
+
+public class MainClass {
+	public static void main(String[] args) {
+		ApplicationContext app=new ClassPathXmlApplicationContext("application.xml");
+		EmpDAO dao=(EmpDAO)app.getBean("dao");
+		List<EmpVO> list=dao.empListData();
+		for(EmpVO vo:list)
+		{
+			System.out.println(vo.getEmpno()+" "+vo.getEname()+" "+vo.getJob()+" "+vo.getSal()+" "+vo.getHiredate());
+		}
+	}
+}
