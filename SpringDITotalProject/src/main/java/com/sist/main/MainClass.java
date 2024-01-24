391e32dd996a523221426012601113417bd0167f
yoonsai <micelle0716@naver.com>
2024-01-24 17:30:06 +0900

    DI정리

---
+++ SpringDITotalProject/src/main/java/com/sist/main/MainClass.java
@@ -0,0 +1,21 @@
+package com.sist.main;
+/*
+ *   1. 스프링에서 제공하는 컨테이너 (클래스 관리)
+ *       => 생명주기 : 객체생성 => 객체 사용 => 객체 소멸 => destory()
+ *                     |        | DL(getBean())
+ *                            ====== 객체 사용만 우리가 사용
+ *                   필요한 데이터가 있는 경우 => DI
+ *                  
+ *       => 스프링 : 결합성이 낮은 프로그램
+ *                 ** 공통기반은 처리
+ *                 ** 핵심기능만 소스 코딩 
+ * 
+ */
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
